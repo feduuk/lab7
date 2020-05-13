57399879bf5d9bc29f5beb3a26113f15a4e5704b
ashindin <shindin@rf.unn.ru>
2020-05-13 11:41:28 +0300

    fixes

--- y''(y').py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-data1=np.loadtxt("file_y2.txt")
-plt.plot(data1[:,0],data1[:,1])
-plt.grid(True)
-plt.show()
-
---
+++ y_dash_x_.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data1=np.loadtxt("file_z.txt")
+plt.plot(data1[:,0],data1[:,1])
+plt.grid(True)
+plt.show()
+
